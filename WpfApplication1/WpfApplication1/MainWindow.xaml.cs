5edb4b5309a7f21f825f99721dad4868ffb57ef6
Danqing Yang <lenaydq@outlook.com>
2016-07-20 21:04:50 +0200

    Text Transformer

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using test;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //textBox1.Text = textBox.Text;
+            Transformer hToV = new Transformer(textBox.Text);
+            string text = hToV.result;
+            textBox1.Text = text.ToString();
+            //throw new NotImplementedException();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Transformer hToV = new Transformer(textBox.Text);
+            string text = hToV.result;
+            //if(textBox1!=null)
+                textBox1.Text = text.ToString();
+            Clipboard.SetText(text);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApplication1/test/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test= "这次句号在中间试试．全角很重要．";
+            for (int i = 0; i < 2;i++)
+            {
+                test = test + test;
+            }
+            Transformer t = new Transformer(test);
+            Console.WriteLine();
+            Console.Write(t.result);
+        }
+    }
+
+    public class Transformer
+    {
+        public string result;
+        private int count;
+        private const int ROW    = 7;
+        private const int COLUMN = 9;
+
+        public Transformer(string text)
+        {
+            count = text.Length;
+            int capacity = ROW * COLUMN;
+            //check the length of input string, if longer than capacity, divide.
+            if (count < capacity)
+                transform(text);
+            else
+            {
+                Console.Write("divide message");
+                int components = (int)Math.Ceiling((double)count / capacity);
+                for (int i = 0; i < components; i++)
+                {
+                    string textPart = text.Substring(i * capacity, capacity);
+                    transform(textPart);
+                }
+            }
+        }
+
+        public void transform(string text)
+        {
+            List<string> inputStrList = new List<string>();
+            //init charList
+            for (int i = 0; i < count; i++)
+            {
+                inputStrList.Add(text.Substring(i, 1));
+            }
+            print(inputStrList);
+
+            result = tranposMatrix(inputStrList);
+
+            return;
+        }
+
+        private string tranposMatrix(List<string> list)
+        {
+            string[,] matrix = new string[ROW,COLUMN];
+            for (int i = 0; i<ROW;i++)
+            {
+                for(int j= 0;j<COLUMN;j++)
+                {
+                    int currChar = i*COLUMN +j;
+                    if (currChar < count)
+                    {
+                        matrix[i,j] = list[currChar];
+                        Console.Write(matrix[i,j]);
+                    }
+                    else
+                    {
+                        string temp = "　";
+                        matrix[i,j] = temp;
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            
+            List<string> outputStrList = new List<string>();
+            Console.Write("\n test: \n");
+            for (int i= 0; i<COLUMN;i++)
+            {
+                for (int j= ROW;j>0;j-- )
+                {
+                    outputStrList.Add(matrix[j-1,i]);
+                    Console.Write(matrix[j - 1, i]+" ");
+                }
+                outputStrList.Add("\n");
+                //Console.WriteLine();
+            }
+            
+            return outPutString(outputStrList);
+        }
+
+        private string outPutString(List<string> l)
+        {
+            string s = "" ;
+            for(int i = 0; i<l.Count(); i++)
+            {
+                s = s + l[i];
+            }
+
+            return s;
+        }
+
+        public void print(List<string> stringList)
+        {
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                Console.Write(stringList[i]+" ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
